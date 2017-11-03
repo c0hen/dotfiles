84bce8fad8ae45440d7c57ab7ee54e8ed9365eda
c0hen <tanelrk@gmail.com>
2017-11-03 18:32:31 +0200

    Walk into my parlour

---
+++ README.md
@@ -0,0 +1,3 @@
+# dotfiles
+
+Ricing on Arch
---
+++ xorg-dynamic/1randr.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#~/bin/1randr.sh
+#xrandr --output DFP9 --primary --auto --output DFP10 --off
+#xrandr --output HDMI-A-0 --primary --auto --set audio off --output DVI-I-0 --off
+xrandr --output HDMI-A-0 --primary --auto --output DVI-I-0 --off
+#xrandr --output HDMI1 --primary --auto --output VGA1 --off
